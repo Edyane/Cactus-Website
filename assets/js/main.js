eb10acab1bfdd06f2e818eb29b4bdb922ba6e92f
Edyane <edyanealves@gmail.com>
2024-06-07 22:33:51 -0300

    feat: adicionando show scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,52 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+	navToggle = document.getElementById("nav-toggle"),
+	navClose = document.getElementById("nav-close");
 
-    // menu show
-    if (navToggle) {
-        navToggle.addEventListener('click', () => {
-            navMenu.classList.add('show-menu')
-        })
-    }
+// menu show
+if (navToggle) {
+	navToggle.addEventListener("click", () => {
+		navMenu.classList.add("show-menu");
+	});
+}
 
-    // menu hidden
-    if (navClose) {
-        navClose.addEventListener('click', () => {
-            navMenu.classList.remove('show-menu')
-        })
-    }
+// menu hidden
+if (navClose) {
+	navClose.addEventListener("click", () => {
+		navMenu.classList.remove("show-menu");
+	});
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
-    const navMenu = document.getElementById('nav-menu')
-    //when we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
-}
+	const navMenu = document.getElementById("nav-menu");
+	//when we click on each nav__link, we remove the show-menu class
+	navMenu.classList.remove("show-menu");
+};
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== ADD BLUR HEADER ===============*/
-const blurHeader = () =>{
-    const header = document.getElementById('header');
+const blurHeader = () => {
+	const header = document.getElementById("header");
 
-    this.scrollY >= 50 ? header.classList.add('blur-header')
-                       : header.classList.remove('blur-header');
-}
-window.addEventListener('scroll', blurHeader);
+	this.scrollY >= 50
+		? header.classList.add("blur-header")
+		: header.classList.remove("blur-header");
+};
+window.addEventListener("scroll", blurHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollUp = () => {
+	const scrollUp = document.getElementById("scroll-up");
+	this.scrollY >= 450
+		? scrollUp.classList.add("show-scroll")
+		: scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
